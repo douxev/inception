780a339d2a999a2a4657f37033e38f138cfb0df4
douxev <jdoukhan@student.42.fr>
2024-04-15 11:00:53 -0400

    working_mandatory

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # service mysql start
-# service mysql start
 /usr/sbin/mysqld &
-# sleep 10
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-mysqladmin -u root -p status
-sleep 10
-mysql -u root -e "CREATE DATABASE ${SQL_DATABASE};"
-mysql -u root -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# sleep 5
+
+source /vars.env
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mysqladmin -u root -p status
+
+# mysql -u root -e "CREATE DATABASE ${SQL_DATABASE} ;"
+# mysql -u root -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+sleep 20
--- srcs/requirements/wordpress/tools/autoconf.sh
+++ srcs/requirements/wordpress/tools/autoconf.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 cd /var/www/wordpress
-# sleep 10
-# cat /var/www/wordpress/wp-config.php
+sleep 10
+
 # exit 12
+/usr/local/bin/wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306
 
-
-# /usr/local/bin/wp config create	--allow-root \
-# 					--dbname=$SQL_DATABASE \
-# 					--dbuser=$SQL_USER \
-# 					--dbpass=$SQL_PASSWORD \
-# 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 /usr/local/bin/wp core install --title=$WP_TITLE \
 				--admin_user=$WP_ADMIN_USER \
@@ -18,18 +17,17 @@ cd /var/www/wordpress
 				--url=$WP_URL \
 				--allow-root
 
-/usr/local/bin/wp core config	\
-				--dbhost=mariadb:3306 --path='/var/www/wordpress' \
-				--dbname=$SQL_DATABASE \
-				--dbuser=$SQL_USER \
-				--dbpass=$SQL_PASSWORD \
-				--allow-root
-
+# /usr/local/bin/wp core config	\
+# 				--dbhost=mariadb:3306 --path='/var/www/wordpress' \
+# 				--dbname=$SQL_DATABASE \
+# 				--dbuser=$SQL_USER \
+# 				--dbpass=$SQL_PASSWORD \
+# 				--allow-root
 
 /usr/local/bin/wp user create $WP_USER $WP_USER_MAIL \
 				--role=author \
 				--user_pass=$WP_USER_PASSWORD\
 				--allow-root
 
-wp redis enable --allow-root
+# wp redis enable --allow-root
 cd -
